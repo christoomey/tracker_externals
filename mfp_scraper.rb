a1439031b4f1fe3db67163451e1837fc5d84e4ad
Chris Toomey <chris@ctoomey.com>
2012-08-26 21:05:22 -0400

    Add water glass count to mfp_scrapper

--- mfp_scraper.rb
+++ mfp_scraper.rb
@@ -14,6 +14,12 @@ def get_exercise user, date
   exercise = Integer doc.css('span.soFar')[2].text
 end
 
+def get_water user, date
+  doc = get_page user, date, 'food'
+  raw_text = doc.css(".water-counter p").children.text
+  water_count = Integer raw_text.gsub(/[up|down]/i, "").strip
+end
+
 def get_page user, date, page
   str_format = "%Y-%m-%d"
   date_str = date.strftime(str_format)
@@ -24,14 +30,15 @@ end
 def main
   user = "christoomey"
   today = Date.today
-  puts sprintf "%10s%10s%10s", "Date", "Calories", "Exercise"
+  puts sprintf "%10s%10s%10s%10s", "Date", "Calories", "Exercise", "Water"
   puts "-"*45
   (0..10).each do |i|
     date = (today - i)
     calories = get_calories user, date
     exercise = get_exercise user, date
+    water = get_water user, date
     date_str = date.strftime("%Y-%m-%d")
-    row_str = sprintf "%10s%10s%10s", date_str, calories, exercise
+    row_str = sprintf "%10s%10s%10s%10s", date_str, calories, exercise, water
     puts row_str
   end
 end
